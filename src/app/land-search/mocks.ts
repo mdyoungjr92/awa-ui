210b6cb225f498e532b7c4216a98d2ae519c7b18
Mike Young <mdyoungjr92@yahoo.com>
2018-09-18 20:57:08 -0500

    Committing mock data

--- src/app/land-search/mocks.ts
+++ src/app/land-search/mocks.ts
@@ -1,19 +1,6 @@
 import { Season } from './season';
 import { StateSeason } from './state-season';
 
-export const STATE_SEASONS : StateSeason[] = [
-  { state: 'Missouri' , seasons: [
-    { id: 1, name: 'Quail Hunting', state: 'MO' },
-    { id: 2, name: 'Pheasant Hunting', state: 'MO' }
-  ] },
-  { state: 'Kansas', seasons: [
-    { id: 2, name: 'Pheasant Hunting', state: 'KS' },
-    { id: 3, name: 'Goose Hunting', state: 'KS' },
-    { id: 4, name: 'Whitetail Deer Hunting', state: 'KS' },
-    { id: 5, name: 'Antelope Hunting', state: 'KS' }
-  ] }
-];
-
 export const SEASONS : Season[] = [
   { id: 1, name: 'Quail Hunting', state: 'MO' },
   { id: 2, name: 'Pheasant Hunting', state: 'MO' },
